b6cffb6979714901a9fdae36fbcb404256d06dfa
MRizzi96 <mark771396@gmail.com>
2020-09-02 16:27:04 +0200

    Spawn Player in Arena risolto
    Arena scene completa.

---
+++ AstralTournament-Merge/Assets/Scripts/Selected/ArenaSelected.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArenaSelected : MonoBehaviour
+{
+    public Button button;
+    public Arena arena;
+    private Global global;
+    // Start is called before the first frame update
+    void Start()
+    {
+        global = Global.Instance;
+        button.onClick.AddListener(onClick);
+    }
+
+    private void onClick()
+    {
+        global.arena = arena.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AstralTournament-Merge/Assets/Scripts/UNET/NetworkVehicle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkVehicle : NetworkBehaviour
+{
+    public int cannon;
+    public int armor;
+    public int engine;
+    public int wheel;
+
+    public float drag;
+
+    public Vector3 velocity;
+
+    //public Vehicle vehicle;
+
+    private GameObject board;
+    public static bool changed;
+    private bool done;
+
+    public int health;
+    public int maxHealth;
+    public List<PowerUp> powerUps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3(0, 0, 0);
+        drag = 0.02f;
+        powerUps = new List<PowerUp>();
+    }
+
+    public void AddPowerUp(PowerUp powerUp)
+    {
+        powerUps.Add(powerUp);
+    }
+
+    public void UsePowerUp(int index)
+    {
+        PowerUp powerUp = powerUps[index];
+        powerUps.RemoveAt(index);
+    }
+
+    public bool IsPowerUpFull()
+    {
+        return powerUps.Count == 4;
+    }
+
+    
+
+
+}
